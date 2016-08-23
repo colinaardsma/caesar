19ed34e4ea0eaf21b0cdfc5f3ddb1f89952eeb44
Colin Aardsma <colin.aardsma@gmail.com>
2016-08-23 00:07:24 -0500

    updated program

--- main.py
+++ main.py
@@ -15,10 +15,55 @@
 # limitations under the License.
 #
 import webapp2
+import cgi
+
+form = """
+<form method="post">
+    <label>Amount to Shift:
+        <input type="number" name="shift" />
+    </label>
+    <label><h1>Enter Some Text</h1>
+        <textarea name="text" style="height: 10em; width: 40em;">%(text)s</textarea>
+    </label><br>
+    <input type="submit" />
+</form>
+"""
+
+def shift(text, shift):
+    text = cgi.escape(text)
+    shift = int(cgi.escape(shift)) % 26
+    new = ""
+    for char in text:
+        if char.isalpha() == True:
+            newCharNum = ord(char) + shift
+            if char.isupper() == True:
+                if newCharNum > ord('Z'):
+                    newCharNum -= 26
+                    newChar = chr(newCharNum)
+                else:
+                    newChar = chr(newCharNum)
+            else:
+                if newCharNum > ord('z'):
+                    newCharNum -= 26
+                    newChar = chr(newCharNum)
+                else:
+                    newChar = chr(newCharNum)
+        else:
+            newChar = char
+        new += newChar
+    return new
 
 class MainHandler(webapp2.RequestHandler):
+    def write_form(self, text=""):
+        self.response.write(form % {"text": text})
+
     def get(self):
-        self.response.write('Hello world!')
+        self.write_form()
+
+    def post(self):
+        text = shift(self.request.get('text'), self.request.get('shift'))
+        self.write_form(text)
+        #self.response.write(text)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
